5b5725c20cf68b1d247eed8daa9ab4edadbad0c2
Bhuvanesh <bhuvaneshwaran.m@hornbillfx.com>
2020-05-31 10:41:23 +0530

    first cut 10TP

--- Assets/Scripts/playerscore.cs
+++ Assets/Scripts/playerscore.cs
@@ -16,6 +16,16 @@ using Newtonsoft.Json.Linq;
 public class playerscore : MonoBehaviour
 {
     public static playerscore _pScore;
+    public int c1;
+    public int e1;
+    public int k1;
+    public int p1;
+    public int r1;
+    public int r2;
+    public int s1;
+    public int s2;
+    public int s3;
+    public string lastTraits;
 
     public int gameSceneId;
     public Text scoretext, Display_txt, Previous_scoretxt;
@@ -38,7 +48,7 @@ public class playerscore : MonoBehaviour
 
     public static float playscore;
     public static string playername;
-    user user = new user();//arthi---
+    public user user = new user();//arthi---
 
     public static string localid;
     private string idtoken;
@@ -58,13 +68,13 @@ public class playerscore : MonoBehaviour
 
     private void OnEnable()
     {
-        if (playerscore._pScore == null)
+        if (_pScore == null)
         {
-            playerscore._pScore = this;
+            _pScore = this;
         }
         else
         {
-            if (playerscore._pScore != this)
+            if (_pScore != this)
             {
                 Destroy(gameObject);
             }
@@ -78,6 +88,11 @@ public class playerscore : MonoBehaviour
         page2.SetActive(false);
         page3.SetActive(false);
 
+        //if (PlayerPrefs.HasKey("Email"))
+        //{
+        //    signInuser(PlayerPrefs.GetString("Email"), PlayerPrefs.GetString("Password"));
+        //}
+
         // playscore = random.Next(0,100);
         // scoretext.text = "score:" + playscore;
     }
@@ -85,9 +100,11 @@ public class playerscore : MonoBehaviour
     public void onsubmit()
     {
         posttodatabase(true);
+        Debug.Log("scoreSubmitted");
     }
     public void ongetscore()
     {
+        Debug.Log("gettingScore");
         GetlocalId();
     }
     private void updatescore()
@@ -100,6 +117,17 @@ public class playerscore : MonoBehaviour
         {
 
             List<int> scr = new List<int>();
+
+            List<int> courage = new List<int>();
+            List<int> ethics = new List<int>();
+            List<int> kindness = new List<int>();
+            List<int> practicality = new List<int>();
+            List<int> responsibility = new List<int>();
+            List<int> risk = new List<int>();
+            List<int> selfcontrol = new List<int>();
+            List<int> silliness = new List<int>();
+            List<int> strategy = new List<int>();
+
             user user = new user();
 
             RestClient.Get(url: databaseURL + "/" + localid + ".json").Then(onResolved: response =>
@@ -108,8 +136,21 @@ public class playerscore : MonoBehaviour
                 if (response != null)
                 {
                     ud = JsonConvert.DeserializeObject<UserDetails>(response.Text);
-                    if (ud != null && ud.userscore!=null)
+                    if (ud != null && ud.userscore != null)
+                    {
                         scr = ud.userscore;
+                        courage = ud.Courage;
+                        ethics = ud.Ethics;
+                        kindness = ud.Kindness;
+                        practicality = ud.Practicality;
+                        responsibility = ud.Responsibility;
+                        risk = ud.Responsibility;
+                        selfcontrol = ud.SelfControl;
+                        silliness = ud.Silliness;
+                        strategy = ud.Strategy;
+
+                    }
+
                 }
                 
 
@@ -118,35 +159,67 @@ public class playerscore : MonoBehaviour
                     if (scr!=null && scr.Count == 10)
                     {
                         scr.RemoveAt(0);
+
+                        courage.RemoveAt(0);
+                        ethics.RemoveAt(0);
+                        kindness.RemoveAt(0);
+                        practicality.RemoveAt(0);
+                        responsibility.RemoveAt(0);
+                        risk.RemoveAt(0);
+                        selfcontrol.RemoveAt(0);
+                        silliness.RemoveAt(0);
+                        strategy.RemoveAt(0);
                     }
-                    scr.Add(Convert.ToInt32(_slider.value));
+
+                    //scr.Add(Convert.ToInt32(_slider.value));
+                    scr.Add(1);
+
+                    courage.Add(c1);
+                    ethics.Add(e1);
+                    kindness.Add(k1);
+                    practicality.Add(p1);
+                    responsibility.Add(r1);
+                    risk.Add(r2);
+                    selfcontrol.Add(s1);
+                    silliness.Add(s2);
+                    strategy.Add(s3);
                 }
                     // playername = usernametext.text;//---page2
-                    user.userName = playername;
-                print("tesy---" + playername);
+                user.userName = playername;
+
                 user.localid = localid;
                 user.userscore = scr;
+
+                user.Courage = courage;
+                user.Ethics = ethics;
+                user.Kindness = kindness;
+                user.Practicality = practicality;
+                user.Responsibility = responsibility;
+                user.Risk = risk;
+                user.SelfControl = selfcontrol;
+                user.Silliness = silliness;
+                user.Strategy = strategy;
+
                 RestClient.Put(url: databaseURL + "/" + localid + ".json", user);
 
-                if (isScoreSubmit)
-                {
-                    submittxt.text = "Score submitted";
+                //if (isScoreSubmit)
+                //{
+                //    submittxt.text = "Score submitted";
 
-                    Invoke("stopmsg", 2f);
-                }
+                //    Invoke("stopmsg", 2f);
+                //}
+                print("tesy---" + playername);
+
             }).Catch(error =>
-            {
-                Debug.Log(error);
+                {
+                    Debug.Log(error);
 
-            });
-
-
+                });
         }
         catch (Exception ex)
         {
 
         }
-
     }
 
     private void retrivefromdatabase()
@@ -158,10 +231,15 @@ public class playerscore : MonoBehaviour
               print("44444");
           });
     }
+
     public void signInUserButton()
     {
+        PlayerPrefs.SetString("Email", emailtext.text);
+        PlayerPrefs.SetString("Password", passwordtext.text);
+
         signInuser(emailtext.text, passwordtext.text);
     }
+
     public void signUpUserButton()
     {
         signUpuser(emailtext1.text, usernametext1.text, passwordtext1.text);
@@ -227,10 +305,10 @@ public class playerscore : MonoBehaviour
     }
     void Shownextpage()
     {                                                                       //edit by Bhuvanesh
-        page1.SetActive(false);
-        page2.SetActive(false);
-        page3.SetActive(true);
-        //SceneManager.LoadScene(gameSceneId);
+        //page1.SetActive(false);
+        //page2.SetActive(false);
+        //page3.SetActive(true);
+        SceneManager.LoadScene(gameSceneId);
                                                                             //edit by Bhuvanesh
         print("page2---" + playername);
         disUsername.text = "Welcome " + playername;
@@ -247,8 +325,8 @@ public class playerscore : MonoBehaviour
             // playername=usernametext.text;
             user.userName = playername;//----
             Display_txt.text = "Welcome " + playername;
-             Invoke("stopmsg", 1f);
-             Invoke("Shownextpage", 1f);
+
+             Invoke("ongetscore", 1f);
 
          }).Catch(error =>
          {
@@ -276,6 +354,7 @@ public class playerscore : MonoBehaviour
             // if (userdata.userName==username)
             //{
             print("3333");
+
             getLocalId = userdata.localid;
             var sb = new StringBuilder();
             if (userdata.userscore != null && userdata.userscore.Count > 0)
@@ -288,8 +367,64 @@ public class playerscore : MonoBehaviour
                 }
             }
             Previous_scoretxt.text = "Previous score:" + "\n" + Convert.ToString(sb);// getscore
-                                                                                     // retrivefromdatabase();
 
+            if (userdata.Courage != null && userdata.Courage.Count > 0)
+            {
+                if (userdata.Courage[userdata.Courage.Count - 1] != 0)
+                    lastTraits += (string.IsNullOrEmpty(lastTraits) ? "" : "\n") + string.Format("{0}: {1}", "Courage", userdata.Courage[userdata.Courage.Count - 1].ToString());
+            }
+
+            if (userdata.Ethics != null && userdata.Ethics.Count > 0)
+            {
+                if (userdata.Ethics[userdata.Ethics.Count - 1] != 0)
+                    lastTraits += (string.IsNullOrEmpty(lastTraits) ? "" : "\n") + string.Format("{0}: {1}", "Ethics", userdata.Ethics[userdata.Ethics.Count - 1].ToString());
+            }
+
+            if (userdata.Kindness != null && userdata.Kindness.Count > 0)
+            {
+                if (userdata.Kindness[userdata.Kindness.Count - 1] != 0)
+                    lastTraits += (string.IsNullOrEmpty(lastTraits) ? "" : "\n") + string.Format("{0}: {1}", "Kindness", userdata.Kindness[userdata.Kindness.Count - 1].ToString());
+            }
+
+            if (userdata.Practicality != null && userdata.Practicality.Count > 0)
+            {
+                if (userdata.Practicality[userdata.Practicality.Count - 1] != 0)
+                    lastTraits += (string.IsNullOrEmpty(lastTraits) ? "" : "\n") + string.Format("{0}: {1}", "Practicality", userdata.Practicality[userdata.Practicality.Count - 1].ToString());
+            }
+
+            if (userdata.Responsibility != null && userdata.Responsibility.Count > 0)
+            {
+                if (userdata.Responsibility[userdata.Responsibility.Count - 1] != 0)
+                    lastTraits += (string.IsNullOrEmpty(lastTraits) ? "" : "\n") + string.Format("{0}: {1}", "Responsibility", userdata.Responsibility[userdata.Responsibility.Count - 1].ToString());
+            }
+
+            if (userdata.Risk != null && userdata.Risk.Count > 0)
+            {
+                if (userdata.Risk[userdata.Risk.Count - 1] != 0)
+                    lastTraits += (string.IsNullOrEmpty(lastTraits) ? "" : "\n") + string.Format("{0}: {1}", "Risk", userdata.Risk[userdata.Risk.Count - 1].ToString());
+            }
+
+            if (userdata.SelfControl != null && userdata.SelfControl.Count > 0)
+            {
+                if (userdata.SelfControl[userdata.SelfControl.Count - 1] != 0)
+                    lastTraits += (string.IsNullOrEmpty(lastTraits) ? "" : "\n") + string.Format("{0}: {1}", "SelfControl", userdata.SelfControl[userdata.SelfControl.Count - 1].ToString());
+            }
+
+            if (userdata.Silliness != null && userdata.Silliness.Count > 0)
+            {
+                if (userdata.Silliness[userdata.Silliness.Count - 1] != 0)
+                    lastTraits += (string.IsNullOrEmpty(lastTraits) ? "" : "\n") + string.Format("{0}: {1}", "Silliness", userdata.Silliness[userdata.Silliness.Count - 1].ToString());
+            }
+
+            if (userdata.Strategy != null && userdata.Strategy.Count > 0)
+            {
+                if (userdata.Strategy[userdata.Strategy.Count - 1] != 0)
+                    lastTraits += (string.IsNullOrEmpty(lastTraits) ? "" : "\n") + string.Format("{0}: {1}", "Strategy", userdata.Strategy[userdata.Strategy.Count - 1].ToString());
+            }
+
+            Debug.Log(lastTraits);
+            Invoke("stopmsg", 1f);
+            Invoke("Shownextpage", 1f);
             //}
             //else
             //{
@@ -315,11 +450,10 @@ public class playerscore : MonoBehaviour
     //
     void Update()
     {
-        //if(Input.GetMouseButtonDown(0))
-        //{
-        //    Display_txt.text = "";
-
-        //}
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
     // 
     public void SignUpPage1()
@@ -328,6 +462,11 @@ public class playerscore : MonoBehaviour
         page2.SetActive(true);
         page3.SetActive(false);
     }
+
+    //public void SignOutUser()
+    //{
+    //    RestClient.
+    //}
 }
 //?auth=" + idtoken
 
