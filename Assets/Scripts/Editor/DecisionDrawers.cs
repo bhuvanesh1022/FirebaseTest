06eec6ba107597757cc6bdfbbf86dac4cda5b472
Projjal Chakrabarti <pc@bigfatphoenix.com>
2020-06-01 21:47:30 +0530

    Added more decisions  - Added UserGameData class for sending trait changes on game end  - More consistent text appear speed across devices

---
+++ Assets/Scripts/Editor/DecisionDrawers.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using DecisionFramework;
+
+[CustomPropertyDrawer(typeof(Property))]
+[CanEditMultipleObjects]
+public class PropertyEditor : PropertyDrawer {
+	const float PROP_HEIGHT = 19f;
+
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+		Rect propRect = position;
+		propRect.height = PROP_HEIGHT;
+		EditorGUI.PropertyField(propRect, property.FindPropertyRelative("type"));
+		propRect.y += PROP_HEIGHT;
+		EditorGUI.PropertyField(propRect, property.FindPropertyRelative("name"));
+	}
+
+	public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+		return PROP_HEIGHT * 2;
+	}
+}
